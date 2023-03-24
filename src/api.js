d0657cd04d1fa25221bcd285e491302a2988a0f4
vasi-h <101004449+Vasi89@users.noreply.github.com>
2023-03-24 23:18:32 +0100

    update api.js

--- src/api.js
+++ src/api.js
@@ -1,8 +1,20 @@
-import axios from 'axios';
-
 const API_KEY = '1cf50e6248dc270629e802686245c2c8';
 
 export const fetchMovies = async () => {
-  const response = await axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=it-IT`);
-  return response.data.results;
+  const response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=it-IT`);
+  const data = await response.json();
+  return data.results;
 };
+
+export const deleteMovie = async (movieId) => {
+  // ... implementation of deleteMovie
+};
+
+export const updateMovie = async (updatedMovie) => {
+  // ... implementation of updateMovie
+};
+
+
+
+
+
